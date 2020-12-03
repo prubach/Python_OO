0ec5e3195f6612b2dd249177d1511c3806e4f3a0
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-12-03 11:24:43 +0100

    Multiinheritance

--- Shapes.py
+++ Shapes.py
@@ -20,14 +20,28 @@ class Rectangle(Shape):
     def calc_surface(self):
         return self._a*self.b
 
+    def calc_perimeter(self):
+        return 2*self._a+2*self.b
+
 import math
 
 class Circle(Shape):
+    def __init__(self, a):
+        super().__init__(a, 0)
+        #self._a = a
+
     def calc_surface(self):
         return math.pi*self._a**2
 
     def __repr__(self):
         return self.__class__.__name__ + "[r=" + str(self._a) + "] at " + str(hex(id(self)))
+
+
+class HybridShape(Rectangle, Circle):
+    pass
+
+class HybridShapeB(Circle, Rectangle):
+    pass
 
 
 r = Rectangle(5, 7)
@@ -38,3 +52,13 @@ print(r.calc_surface())
 s = Circle(50)
 print(s)
 print(s.calc_surface())
+
+hs = HybridShape(5)
+print(hs)
+print(hs.calc_surface())
+print(hs.calc_perimeter())
+
+hsb = HybridShapeB(10)
+print(hsb)
+print(hsb.calc_surface())
+print(hsb.calc_perimeter())
