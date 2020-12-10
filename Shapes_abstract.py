1a133ad557307173e26b19313e8a0f66fb581661
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-12-10 10:15:07 +0100

    Shape - as abstract class

---
+++ Shapes_abstract.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    def __init__(self, a=10, b=6):
+        self.set_params(a, b)
+
+    @abstractmethod
+    def calc_surface(self):
+        pass
+
+    def set_params(self, a, par_b):
+        self._a = a
+        self.b = par_b
+
+    def get_a(self):
+        return self._a
+
+    def __repr__(self):
+        return self.__class__.__name__ + "[" + str(self._a) + " by " + str(self.b) + "] at " + str(hex(id(self)))
+
+
+class Rectangle(Shape):
+    def calc_surface(self):
+        return self._a*self.b
+
+    def swap_sides(self):
+        a = self._a
+        b = self.b
+        self._a = b
+        self.b = a
+
+import math
+
+class Circle(Shape):
+    def __init__(self, a):
+        # call constructor of superclass (parent)
+        super().__init__(a, 0)
+        #self._a = a
+
+    def calc_surface(self):
+        return math.pi * self._a**2
+
+    def __repr__(self):
+        return self.__class__.__name__ + "[r=" + str(self._a) + "] at " + str(hex(id(self)))
+
+
+#s = Shape(14, 4)
+#print(s)
+
+
+
+r = Rectangle(5, 6)
+print(r)
+#r._a = 600
+#print(r.calc_surface())
+r.swap_sides()
+print(r)
+
+c = Circle(7)
+print(c)
+print(c.calc_surface())
