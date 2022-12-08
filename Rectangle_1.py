d713d0040f371dc5c301af3d00bd00692a1c903c
Paweł Rubach <pawel.rubach@gmail.com>
2022-12-08 11:27:23 +0100

    Naming of rects and lists should be clearer

--- Rectangle_1.py
+++ Rectangle_1.py
@@ -17,57 +17,55 @@ class Rectangle:
     #def __str__(self):
     #    return 'Rectangle[{} by {} at {}]'.format(self.a, self.b, hex(id(self)))
 
-r = Rectangle()
-r.set_params(4, 5)
-print(r.calc_surface())
-print('R: {}'.format(r))
+r1 = Rectangle()
+r1.set_params(4, 5)
+print(r1.calc_surface())
+print('R1: {}'.format(r1))
 #
 r2 = Rectangle()
 r2.set_params(6, 7)
 print(r2.calc_surface())
 print('R2: {}'.format(r2))
 
-r3 = r
-print('R3: {}'.format(r3))
-
-from copy import copy
-r4 = copy(r2)
-print('R2=R4: {}'.format(r2==r4))
-print('R2 compare R4: {}'.format(r2.compare(r4)))
-print('R1=R3: {}'.format(r==r3))
-print('R1 compare R3: {}'.format(r.compare(r3)))
-
-print('R4: {}'.format(r4))
+# r3 = r1
+# print('R3: {}'.format(r3))
+# #
+# from copy import copy
+# r4 = copy(r2)
+# print('R4: {}'.format(r4))
+# print('R1=R3: {}'.format(r1==r3))
+# print('R2=R4: {}'.format(r2==r4))
+# print('R2 compare R4: {}'.format(r2.compare(r4)))
+# print('R1=R3: {}'.format(r1==r3))
+# print('R1 compare R3: {}'.format(r1.compare(r3)))
 #
-r_list = [r, r2, r3, r4]
-print('r_list:')
-print(r_list)
-r2_list = r_list
-print('r2_list:')
-print(r2_list)
-r3_list = r_list.copy()
-print('r3_list:')
-r_list[0].set_params(9, 8)
-print(r3_list)
-print(r2_list)
-print(r)
-from copy import deepcopy
-r4_list = deepcopy(r_list)
-r4_list[0].set_params(10, 15)
-print('r4_list:')
-print(r4_list)
-print('r2_list:')
-print(r2_list)
+# print('R4: {}'.format(r4))
+# #
+# lr1_list = [r1, r2, r3, r4]
+# print(f'LR1_list:{lr1_list}')
+# lr2_list = lr1_list
+# print(f'LR2_list:{lr2_list}')
+# lr3_list = lr1_list.copy()
+# print('r3_list:')
+# print(lr3_list)
+# lr1_list[0].set_params(9, 8)
+# print(f'LR1_list:{lr1_list}')
+# print(f'LR2_list:{lr2_list}')
+# print(f'LR3_list:{lr3_list}')
+# from copy import deepcopy
+# lr4_list = deepcopy(lr1_list)
+# print(f'LR4_list:{lr4_list}')
+# lr4_list[0].set_params(10, 15)
+# print(f'LR1_list:{lr1_list}')
+# print(f'LR2_list:{lr2_list}')
+# print(f'LR3_list:{lr3_list}')
+# print(f'LR4_list:{lr4_list}')
 
-#
-#
 # print('changing params')
-# r2.set_params(8, 9)
-# print(r_list)
-# print(r2_list)
-# print('r4_list:')
-# print(r4_list)
+#r2.set_params(3, 2)
 #
 # #del r4_list
-# #print('r4_list:')
-# #print(r4_list)
+# print(f'R_list:{r_list}')
+# print(f'R2_list:{r2_list}')
+# print(f'R3_list:{r3_list}')
+# print(f'R4_list:{r4_list}')
